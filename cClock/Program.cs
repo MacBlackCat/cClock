dc237632dc950a1cf9b0fe1a602eb21c4f628f11
MacBlackCat <jelmer.oldersma@gmail.com>
2018-01-24 14:26:15 +0100

    Digital clock is functional (single use though)

--- cClock/Program.cs
+++ cClock/Program.cs
@@ -10,6 +10,36 @@ namespace cClock
     {
         static void Main(string[] args)
         {
+            //debug console (first check if its correctly startups
+            Console.WriteLine("Iam doing something!");
+
+            //the NOT analogue clock heheh
+            AnaClock clock;
+            clock = new AnaClock();
+            Console.WriteLine(clock.Time());
+
+            //Just to keep console open till I want it closed ;)
+            Console.ReadKey();
+        }
+    }
+
+    class AnaClock
+    {
+        public AnaClock()
+        {
+            this.time = GetTime;
+        }
+        
+        private string time;
+        public string GetTime
+        {
+            get { return DateTime.Now.ToLongTimeString(); }
+            set { time = value; }
+        }
+
+        public string Time()
+        {
+            return GetTime;
         }
     }
 }
